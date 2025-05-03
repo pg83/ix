5d920419f5683ad64fff1c3b44561634af9155b4
Andrew Grigorev <andrew@ei-grad.ru>
2025-05-03 16:59:46 +0300

    fix

--- pkgs/bin/ix/tools/regen/scripts/repo_export.sh
+++ pkgs/bin/ix/tools/regen/scripts/repo_export.sh
@@ -2,4 +2,4 @@
 
 set -xue
 
-IX_EXEC_KIND=fake IX_DUMP_REPO=1 ./ix build set/ci --seed=1 | ix_repo | sort | uniq | ix_flt | sort | uniq > pkgs/die/scripts/dump.json
+IX_EXEC_KIND=fake IX_DUMP_REPO=1 IX_SEED=1 ./ix build set/ci | ix_repo | sort | uniq | ix_flt | sort | uniq > pkgs/die/scripts/dump.json
