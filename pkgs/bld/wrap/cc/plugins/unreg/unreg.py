4b066bd226ce7ec6bc4a32fe8d20b5df8d964dcf
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-01-12 18:28:33 +0300

    fix build

--- pkgs/bld/wrap/cc/plugins/unreg/unreg.py
+++ pkgs/bld/wrap/cc/plugins/unreg/unreg.py
@@ -3,9 +3,6 @@ import sys
 import json
 import subprocess
 
-if os.environ.get('IX_STEP', '') == 'configure':
-    sys.exit(0)
-
 args = json.loads(sys.stdin.read())['cmd']
 
 def it_bins():
@@ -16,6 +13,9 @@ def it_bins():
             yield x
 
 def need_plugins():
+    if os.environ.get('IX_STEP', '') == 'configure':
+        return False
+
     if '-o' in args:
         o = args[args.index('-o') + 1]
     else:
