d6b3f0336b53bf5e38f99a65029c67d3b216fb3d
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-01-26 15:49:18 +0300

    fix

--- pkgs/lib/std/ix.sh
+++ pkgs/lib/std/ix.sh
@@ -1,27 +1,7 @@
-{% extends '//die/c/make.sh' %}
-
-{% block fetch %}
-https://github.com/pg83/std/archive/refs/tags/2.tar.gz
-a1fa236c731f4b75444d30f9a88a7b7bd1696510e893beba5787cbd4ace19590
-{% endblock %}
+{% extends '//die/hub.sh' %}
 
 {% block lib_deps %}
-lib/xxhash
-{% endblock %}
-
-{% block install %}
-mkdir ${out}/lib
-mkdir ${out}/include
-rm -rf tst
-cp std/libstd.a ${out}/lib/
-find . -type d | while read l; do
-    mkdir -p ${out}/include/${l}
-done
-find . -type f -name '*.h' | while read l; do
-    cp ${l} ${out}/include/${l}
-done
-{% endblock %}
-
-{% block make_target %}
-std/libstd.a
+lib/c
+lib/c++
+lib/std/naked
 {% endblock %}
---
+++ pkgs/lib/std/naked/ix.sh
@@ -0,0 +1,28 @@
+{% extends '//die/c/make.sh' %}
+
+{% block fetch %}
+https://github.com/pg83/std/archive/refs/tags/2.tar.gz
+a1fa236c731f4b75444d30f9a88a7b7bd1696510e893beba5787cbd4ace19590
+{% endblock %}
+
+{% block lib_deps %}
+lib/c++/dispatch
+lib/xxhash/naked
+{% endblock %}
+
+{% block install %}
+mkdir ${out}/lib
+mkdir ${out}/include
+rm -rf tst
+cp std/libstd.a ${out}/lib/
+find . -type d | while read l; do
+    mkdir -p ${out}/include/${l}
+done
+find . -type f -name '*.h' | while read l; do
+    cp ${l} ${out}/include/${l}
+done
+{% endblock %}
+
+{% block make_target %}
+std/libstd.a
+{% endblock %}
