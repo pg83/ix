5723989023e8c671b6ba43d03ff2de8d8cf5e4a4
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-02-04 19:04:54 +0300

    fix

--- pkgs/set/pg/vpn/ix.sh
+++ pkgs/set/pg/vpn/ix.sh
@@ -2,5 +2,5 @@
 
 {% block run_deps %}
 bin/openssh/client
-etc/services/runit(srv_deps=bin/openssh/client,srv_dir=vpn,srv_user=nobody,srv_command=exec ssh -N -D 1080 root@176.74.218.69 -o StrictHostKeyChecking=no {{vpn_password}})
+etc/services/runit(srv_deps=bin/openssh/client,srv_dir=vpn,srv_user=pg,srv_command=exec ssh -N -D 192.168.100.64:1080 root@176.74.218.69 -o StrictHostKeyChecking=no)
 {% endblock %}
