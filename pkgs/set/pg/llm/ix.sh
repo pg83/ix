b892543334c40974c66f4e9806ac8776668293a2
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-05-08 02:35:18 +0300

    better

--- pkgs/set/pg/llm/ix.sh
+++ pkgs/set/pg/llm/ix.sh
@@ -5,19 +5,30 @@ set/pg/llm/libs
 {% endblock %}
 
 {% block run_deps %}
-bld/sh
-bld/box
+bin/sh
 bin/gdb
-bld/make
+bin/sed
+bin/make
+bin/perf
+bin/patch
+bin/wirez
 bin/strace
 set/dev/cc
 set/dev/cc
 set/dev/go
 bin/ollama
 bin/logcli
+bin/bash/5
 bin/tcpdump
 bin/etcd/ctl
 bin/python/14
 bin/ip/route2
+bin/coreutils
+bin/diffutils
+bin/findutils
+bin/gawk/lite
+bin/file/host
+bin/grep/patched
+bin/grep/scripts
 set/pg/llm/libs(kind=lib)
 {% endblock %}
