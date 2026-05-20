b6411c407be73f272c1d25a42ffc1164b7abba19
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-05-20 14:44:31 +0000

    fix

--- pkgs/bin/claude/code/wrap/ix.sh
+++ pkgs/bin/claude/code/wrap/ix.sh
@@ -1,9 +1,5 @@
 {% extends '//die/hub.sh' %}
 
-{% block pkg_name %}
-claude
-{% endblock %}
-
 {% block run_deps %}
 bin/wrap
 bin/claude/code
--- pkgs/bin/wrap/ix.sh
+++ pkgs/bin/wrap/ix.sh
@@ -1,7 +1,6 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-bin/wrap/scripts
-bin/python/14
 bin/jail
+bin/wrap/scripts
 {% endblock %}
