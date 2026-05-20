bb4c3c91b815dbf93429e0e18e9108b715cafe56
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-05-20 15:03:48 +0000

    bin/codex: switch to prebuilt musl release

--- pkgs/bin/codex/ix.sh
+++ pkgs/bin/codex/ix.sh
@@ -1,46 +1,25 @@
-{% extends '//die/rust/cargo.sh' %}
+{% extends '//die/std/ix.sh' %}
 
 {% block pkg_name %}
 codex
 {% endblock %}
 
 {% block version %}
-0.130.0
+0.132.0
 {% endblock %}
 
-{% block cargo_url %}
-https://github.com/openai/codex/archive/refs/tags/rust-v{{self.version().strip()}}.tar.gz
+{% block fetch %}
+https://github.com/openai/codex/releases/download/rust-v{{self.version().strip()}}/codex-x86_64-unknown-linux-musl.tar.gz
+8b64432ee4ef5b1d7d197aad4535a276bc85223f4e4163769c0e1015cda883b2
 {% endblock %}
 
-{% block cargo_sha %}
-303091e36d796b056966b5534f925823a1998fff443313fe4c7a764ab7c424f5
-{% endblock %}
-
-{% block cargo_refine_unpack %}
-mv codex-rs ../__sub__
-find . -mindepth 1 -delete
-find ../__sub__ -mindepth 1 -maxdepth 1 -exec mv {} . \;
-rmdir ../__sub__
-{% endblock %}
-
-{% block bld_libs %}
-lib/c
-lib/openssl
-{% endblock %}
-
-{% block cargo_packages %}
-codex-cli
-{% endblock %}
-
-{% block cargo_bins %}
-codex
-{% endblock %}
-
-{% block cargo_tool %}
-bld/cargo/91
+{% block step_unpack %}
+mkdir src
+cd src
+extract0 ${src}/codex-x86_64-unknown-linux-musl.tar.gz
 {% endblock %}
 
 {% block install %}
-{{super()}}
-mv ${out}/bin/codex ${out}/bin/codex.exe
+mkdir -p ${out}/bin
+install -Dm755 codex-x86_64-unknown-linux-musl ${out}/bin/codex.exe
 {% endblock %}
