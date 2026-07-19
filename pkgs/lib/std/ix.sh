7286753a3b09299ba946c1465fe7ce4f8f63f428
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-07-19 16:42:54 +0000

    lib/std: update and switch to pybuild

--- pkgs/lib/std/ix.sh
+++ pkgs/lib/std/ix.sh
@@ -1,8 +1,8 @@
-{% extends '//die/c/make.sh' %}
+{% extends '//die/c/pybuild.sh' %}
 
 {% block fetch %}
-https://github.com/pg83/std/archive/6e1ec7b5543e77527f48d2567c7667dc9b2935e8.tar.gz
-27f3d9d9e2f9dbac3d4ecb22ed6fdc27d4136d545c4aedecc11e3fbe4df458da
+https://github.com/pg83/std/archive/a657c2667d0aa3080a472015fa9e3cd22dc58bd5.tar.gz
+9de7b47b6eef5ad437035c1879e4a8c26877f9b3eb4fd2fca13e9cfb028d1968
 {% endblock %}
 
 {% block lib_deps %}
@@ -14,19 +14,18 @@ lib/rapidhash
 lib/linux/headers
 {% endblock %}
 
+{% block pybuild_target %}
+libstd
+{% endblock %}
+
 {% block install %}
 mkdir ${out}/lib
 mkdir ${out}/include
-rm -rf tst
-cp std/libstd.a ${out}/lib/
-find . -type d | while read l; do
+cp libstd ${out}/lib/libstd.a
+find std -type d | while read l; do
     mkdir -p ${out}/include/${l}
 done
-find . -type f -name '*.h' | while read l; do
+find std -type f -name '*.h' | while read l; do
     cp ${l} ${out}/include/${l}
 done
-{% endblock %}
-
-{% block make_target %}
-std/libstd.a
 {% endblock %}
