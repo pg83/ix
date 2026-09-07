1f7fdf0889172ac7164ce9821d3231235fe0d1e0
Versifikator <324678893+versifikator[bot]@users.noreply.github.com>
2026-09-07 12:32:35 +0000

    up 2.19.1 2.20.0 lib/wacom/data lib/wacom

--- pkgs/lib/wacom/t/ix.sh
+++ pkgs/lib/wacom/t/ix.sh
@@ -5,12 +5,12 @@ libwacom
 {% endblock %}
 
 {% block version %}
-2.19.1
+2.20.0
 {% endblock %}
 
 {% block fetch %}
 https://github.com/linuxwacom/libwacom/releases/download/libwacom-{{self.version().strip()}}/libwacom-{{self.version().strip()}}.tar.xz
-a1e5b1e7ef60fa70ed05b55d888d980ec7e86bd15594857f3c48c529b661bf32
+370b45b5e05a91960df0aeb9c9481ae05846aab92ab2d4ec66945a0da4216888
 {% endblock %}
 
 {% block lib_deps %}
