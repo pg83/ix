821ef8928b1d9b5a5ab9b52c91658a7e79b08f27
Versifikator <324678893+versifikator[bot]@users.noreply.github.com>
2026-09-07 20:53:44 +0000

    up 9.1.0 9.1.1 lib/simd/utf

--- pkgs/lib/simd/utf/ix.sh
+++ pkgs/lib/simd/utf/ix.sh
@@ -5,12 +5,12 @@ simdutf
 {% endblock %}
 
 {% block version %}
-9.1.0
+9.1.1
 {% endblock %}
 
 {% block fetch %}
 https://github.com/simdutf/simdutf/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-24e3510a4c95a9e6eb0fb4a27eea650d13773231cbd8b564ed9670aa5484d193
+ec707f17e5083999efbdaf8a9a08d35e71e955b35dbf4b8307d14a7d31e9697f
 {% endblock %}
 
 {% block lib_deps %}
