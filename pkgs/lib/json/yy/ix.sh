8f38f494600ae0be400c3a96b065c186e9346a1f
Versifikator <324678893+versifikator[bot]@users.noreply.github.com>
2026-09-09 00:17:54 +0000

    up 0.12.0 0.13.0 lib/json/yy

--- pkgs/lib/json/yy/ix.sh
+++ pkgs/lib/json/yy/ix.sh
@@ -5,12 +5,12 @@ yyjson
 {% endblock %}
 
 {% block version %}
-0.12.0
+0.13.0
 {% endblock %}
 
 {% block fetch %}
 https://github.com/ibireme/yyjson/archive/refs/tags/{{self.version().strip()}}.tar.gz
-b16246f617b2a136c78d73e5e2647c6f1de1313e46678062985bdcf1f40bb75d
+34e0f62a2bc11ab20d601e8ca1cc2b2079503aa45119a19133d89d19b94a0fae
 {% endblock %}
 
 {% block lib_deps %}
