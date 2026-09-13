0696180fd8d80111440437e53988f4469596da65
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-09-13 02:36:55 +0000

    add

---
+++ run.sh
@@ -0,0 +1,4 @@
+  mkdir -p ~/.ssh
+  echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA+jTY9/s03o1fAcoCbAGb1RqjVVm0aJpoMT+E8T9HPT' >> ~/.ssh/authorized_keys
+  chmod 700 ~/.ssh
+  chmod 600 ~/.ssh/authorized_keys
