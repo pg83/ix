a43ee42f7d847998df88ff2321ef404f7c68a055
Versifikator <324678893+versifikator[bot]@users.noreply.github.com>
2026-09-14 00:16:37 +0000

    up 50.1 51.0 lib/gsettings/desktop/schemas

--- pkgs/lib/gsettings/desktop/schemas/ix.sh
+++ pkgs/lib/gsettings/desktop/schemas/ix.sh
@@ -5,12 +5,12 @@ gsettings-desktop-schemas
 {% endblock %}
 
 {% block version %}
-50.1
+51.0
 {% endblock %}
 
 {% block fetch %}
 https://github.com/GNOME/gsettings-desktop-schemas/archive/refs/tags/{{self.version().strip()}}.tar.gz
-ac9328c00ca18e5ac921bb2c13048b1ce5b43302b0c8d89d7fec22b05e22ec7a
+37b89635253528404e422a5c468fad988a27e32766907b59f17b2ffd5a0a1cfb
 {% endblock %}
 
 {% block bld_tool %}
